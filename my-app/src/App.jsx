e738f94085eac5718a8824fbde48fc5443362bbe
80011_Toan <vantoan.tran@spclt.com.vn>
2025-09-04 15:49:34 +0700

    first commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-red-400 font-bold'>Hello world</div>
+  )
+}
+
+export default App
