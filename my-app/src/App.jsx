c388ec44f66237cc06e36350bd2155a9e16a735b
80011_Toan <vantoan.tran@spclt.com.vn>
2025-10-23 15:24:15 +0700

    update 23/10

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,7 +6,17 @@ import './App.css'
 function App() {
 
   return (
-    <div className='text-red-400 font-bold'>Hello world</div>
+    <div className="App">
+      <div className='text-red-400 font-bold '>Hello world</div>
+      <button
+        className='curso'
+        onClick={() => {
+          let age = prompt('How old are you?', 100);
+          alert(`You are ${age} years old!`);
+          //alert("6"/"2");
+        }}
+      >Click me!</button>
+    </div>
   )
 }
 
